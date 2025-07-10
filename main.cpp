f0f56f6b81eef2dd963f80e253c5e76a949c8691
Fire Coin <maxim.m.kollar@gmail.com>
2025-07-10 19:13:33 +0200

    Now it displays all the chunks of png and their lenghts.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <cstring>
 
 using namespace std;
+
+uint32_t swapEdian(uint32_t value) {
+    return (((value & 0xFF000000) >> 24) |
+            ((value & 0x00FF0000) >> 8) |
+            ((value & 0x0000FF00) << 8) | 
+            ((value & 0x000000FF) << 24));
+}
+
+
+/* Chunk structure
+    Length: 4 bytes of uint32_t
+    Type: 4 bytes of (ASCII) letters
+    Data: Lenght bytes 
+    CRC: 4 bytes
+*/
 
 int main() {
     fstream fs;
@@ -16,7 +32,36 @@ int main() {
         for (int i = 0; i < 8; ++i)
             cout << static_cast<int>(sign[i]) << ' ';
         cout << endl;
+        
+        unsigned char type[5] = {0}; // Setting all characters to 0
+        unsigned char crc[4];
 
+        do {
+            uint32_t chunkLength = 0;
+
+            // Reading chunk length
+            fs.read(reinterpret_cast<char*>(&chunkLength), sizeof(chunkLength));
+            // Reading chunk type
+            fs.read(reinterpret_cast<char*>(type), 4);
+            type[4] = '\0';
+
+            // Swaping Edian, because png writes values as MSB is on the right side
+            chunkLength = swapEdian(chunkLength);
+
+            unsigned char data[chunkLength] = {0};
+            // Reading chunk data
+            fs.read(reinterpret_cast<char*>(data), chunkLength);
+            // Reading chunk CRC
+            fs.read(reinterpret_cast<char*>(crc), 4);
+
+            for (int i = 0; i < 4; ++i)
+            cout << type[i];
+            cout << endl;
+            
+            cout << "Lenght: " << chunkLength  << " bytes" << endl;
+            cout << '\n';
+    } while (strcmp(reinterpret_cast<const char*>(type), "IEND") != 0);
+        
         fs.close(); // Closing image file
     } else 
         cerr << "Could not open file\n";
